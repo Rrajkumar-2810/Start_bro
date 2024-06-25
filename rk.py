0f7ab1ef440fb5c01e31638bcf70bcc72c86e1eb
Rrajkumar786 <rishirajkumar28oct@gmail.com>
2024-06-25 23:21:29 +0530

    naming

---
+++ rk.py
@@ -0,0 +1,6 @@
+import random
+a = int(input("Enter a number: "))
+print("You must be wondering what operations are going to be performed on it. Right?")
+print("Well, let's see what's in store for you.")
+g=random.random()
+print()
