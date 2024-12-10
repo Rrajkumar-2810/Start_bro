8881aa4116329c243d6ef6f1e015b85085dd9468
Rrajkumar786 <rishirajkumar28oct@gmail.com>
2024-12-10 20:36:09 +0530

    update_numpy_pandas

--- num_py4.py
+++ num_py4.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 a = np.array([5,4,3])
 print(a[0])
 print(a.itemsize) #IntegerSize
 print(a.dtype) #DataType
 print("A's no. of elements=",a.size) #Number of elements within the array
 print("A's dimension:",a.ndim) #Dimension
-print("A's shape=", a.shape)
+print("A's shape=", a.shape) #Rows and columns
 print("a=",a)
 
 b = np.array([[4,2,1],[7,3,9],[6,5,0]])
@@ -15,7 +16,7 @@ print("B's dimensional:",b.ndim) #Dimension
 print("B's IntegerSize",b.itemsize) #IntegerSize
 print("B's datatype=",b.dtype) #DataType
 print("B's no.of elements=",b.size) #Number of elements within the array
-print("B's shape=", b.shape)
+print("B's shape=", b.shape) #Rows and columns
 print("b=",b)
 
 b = np.array([[4,2,1],[7,3,9],[6,5,0]], dtype=np.float64)
@@ -25,6 +26,7 @@ print("b=",b)
 
 c = np.array([[4,2,1],[7,3,9],[6,5,0]], dtype=complex)
 print("c=", c)
-print("C's dimension=",c.ndim)
-print("C's shape=",c.shape)
-print("C's no.of elements=",c.size)
+print("C's dimension=",c.ndim) #Dimension
+print("C's shape=",c.shape) #Rows and columns
+print("C's no.of elements=",c.size) #Number of elements within the array
+
--- pandas1.py
+++ pandas1.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
 df = pd.read_csv("D:/PYVSCODE/Start_bro/nyc_weather.csv")
-#print(df)
+print(df)
 
-print("Max temperature=",df['Temperature'].max())
-print("df shape=", df.shape)
+print("Max temperature=",df['Temperature'].max()) #MAX temperature
+print("Min temperature=", df['Temperature'].min()) #MIN temperature
+print("df shape=", df.shape) # Rows and columns
+
+
+print("Max dew point=",df['DewPoint'].max()) #MAX Dewpoint
+print("Min dew point=",df['DewPoint'].min()) #MIN Dewpoint
+print("Range of dew point=",df['DewPoint'].max()- df['DewPoint'].min()) #Range of Dewpoint
+
+print("Max humidity=",df['Humidity'].max()) #Max humidity
+print("Min humidity=",df['Humidity'].min()) #Min humidity
+print("Range of Humidity=",df['Humidity'].max()-df['Humidity'].min()) #Range of Humidity
+
