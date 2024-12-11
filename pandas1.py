45d15ee7294a5f7f9e7e1fa39369d57442a9bd7b
Rrajkumar786 <rishirajkumar28oct@gmail.com>
2024-12-11 20:22:45 +0530

    Weather_pandas2

--- pandas1.py
+++ pandas1.py
@@ -1,18 +1,27 @@
 import pandas as pd
+#DataFrame is a main object in pandas. Used to represent data in the form of rows and columns
 
-df = pd.read_csv("D:/PYVSCODE/Start_bro/nyc_weather.csv")
+df = pd.read_csv("D:/PYVSCODE/Start_bro/nyc_weather.csv") #Using a variable to access and store the data from the CSV file.
 print(df)
 
-print("Max temperature=",df['Temperature'].max()) #MAX temperature
-print("Min temperature=", df['Temperature'].min()) #MIN temperature
+print("Max temperature=",df['Temperature'].max()) #Max temperature
+print("Min temperature=", df['Temperature'].min()) #Min temperature
+print("Mean Temperature=", df['Temperature'].mean()) #Mean temperature
 print("df shape=", df.shape) # Rows and columns
 
-
-print("Max dew point=",df['DewPoint'].max()) #MAX Dewpoint
-print("Min dew point=",df['DewPoint'].min()) #MIN Dewpoint
+print("Max dew point=",df['DewPoint'].max()) #Max Dewpoint
+print("Min dew point=",df['DewPoint'].min()) #Min Dewpoint
+print("Mean Dewpoint=", df['DewPoint'].mean()) #Mean Dewpoint
 print("Range of dew point=",df['DewPoint'].max()- df['DewPoint'].min()) #Range of Dewpoint
 
 print("Max humidity=",df['Humidity'].max()) #Max humidity
 print("Min humidity=",df['Humidity'].min()) #Min humidity
+print("Mean humidity=", df['Humidity'].mean()) #Mean humidity
 print("Range of Humidity=",df['Humidity'].max()-df['Humidity'].min()) #Range of Humidity
+
+print("Dates on which it rained:\n",df['EST'][df['Events']=='Rain'])
+print("WindSpeed Mean=",df['WindSpeedMPH'].mean()) #Wind Speed Mean before filling null values with zero
+
+print("Null values=",df.fillna(0, inplace=True))
+print("WindSpeed Mean=",df['WindSpeedMPH'].mean()) #Wind Speed Mean after Filling null values with zero
 
---
+++ pandas2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv("D:/PYVSCODE/Start_Bro/weather_data.csv")
+print(df)
+print('Shape:', df.shape)
+rows, columns = df.shape
+print('Columns:', columns)
+print('Describe:', df.describe())
+print('Rainy days:\n', df['day'][df['event']=='Rain'])
+print('Sunny days:\n', df['day'][df['event']=='Sunny'])
+print('Snowy days:\n', df['day'][df['event']=='Snow'])
+
+print('Max temperature:', df['temperature'].max()) #Max temperature
+print('Min temperature:', df['temperature'].min()) #Min temperature
+
+print('Max wind speed:', df['windspeed'].max()) #Max wind speed
+print('Min wind speed:', df['windspeed'].min()) #Min wind speed
+
+print("Head:\n", df.head(2)) #prints all the data records from heading to the count of record number mentioned.
+print("Tail:\n", df.tail(2)) #prints all the data records from last record to the count of record number mentioned
+
+print(df.columns) # Prints all the columns name 
+print(df['event'])
+print("Event with date:\n",df[['event','day']])
+print("Event with date and temperature:\n",df[['event','day','temperature']])
+print("Temperature greater than 32:\n",df[df.temperature >= 32]) #df[df['temperature]]
+print("Max Temperature record:\n",df[df.temperature == df['temperature'].max()])
+print("The day on which temperature was maximum:\n", df[['day','temperature']][df.temperature == df['temperature'].max()])
+
+#Indexing
+print("Setting index as day",df.set_index('day', inplace=True)) #Setted day as index
+print(df)
+print("Resetting index:",df.reset_index(inplace=True)) #Resetting index
+print(df)
+
+print("Setting index as event",df.set_index('event', inplace=True)) #Setted event as index
+print(df)
+print(df.loc['Snow'])
+print("Resetting index:", df.reset_index(inplace=True)) #Resetting index back to original
+print(df)
+
